2a98a164b52bc7f82ffa0be5242b69e296074f10
Azad Astro <azad.astro@gmail.com>
2024-12-01 08:57:56 -0500

    prop 1 2024 clean up

--- 2024/1.py
+++ 2024/1.py
@@ -1,11 +1,11 @@
 import re
-import numpy as np
 
 with open("2024/inputs/1.txt", "r") as f:
     lines = f.readlines()
 
 
-def part_one():
+def read_lists():
+
     left_list = []
     right_list = []
     for line in lines:
@@ -13,25 +13,25 @@ def part_one():
         left_list.append(int(numbers[0]))
         right_list.append(int(numbers[1]))
 
-    s_left = sorted(left_list)
-    s_rght = sorted(right_list)
-    print(sum([abs(l - r) for l, r in zip(s_left, s_rght)]))
+    return left_list, right_list
 
 
-def part_two():
-    left_list = []
-    right_list = []
-    for line in lines:
-        numbers = re.findall(r"\d+", line)
-        left_list.append(int(numbers[0]))
-        right_list.append(int(numbers[1]))
+def part_one(left_list, right_list):
+
+    print(sum([abs(l - r) for l, r in zip(sorted(left_list), sorted(right_list))]))
+
+
+def part_two(left_list, right_list):
 
     print(sum([l * right_list.count(l) for l in left_list]))
 
 
 if __name__ == "__main__":
+
+    left_list, right_list = read_lists()
+
     print("Part One:")
-    part_one()
+    part_one(left_list, right_list)
 
     print("Part Two:")
-    part_two()
+    part_two(left_list, right_list)
