6b41d936b38ae1c4d2d3e8fb408a3251cf548728
Azad Astro <azad.astro@gmail.com>
2024-12-02 09:45:39 -0500

    prob2 2024

--- 2024/2.py
+++ 2024/2.py
@@ -3,13 +3,8 @@ with open("2024/inputs/2.txt", "r") as f:
 
 
 def is_safe(line_list):
-
-    for num1, num2 in zip(line_list, line_list[1:]):
-        d = num1 - num2
-        if d < 1 or d > 3:
-            return False
-
-    return True
+    dist = [n1 - n2 for n1, n2 in zip(line_list, line_list[1:])]
+    return all(1 <= d <= 3 for d in dist) or all(-3 <= d <= -1 for d in dist)
 
 
 def part_one():
@@ -17,8 +12,6 @@ def part_one():
     count = 0
     for line in lines:
         line_list = [int(ch) for ch in line.split()]
-        if line_list[0] - line_list[1] < 0:
-            line_list = line_list[::-1]
 
         if is_safe(line_list):
             count += 1
@@ -30,20 +23,14 @@ def part_two():
     count = 0
     for line in lines:
         line_list = [int(ch) for ch in line.split()]
-        if line_list[0] - line_list[1] < 0:
-            line_list = line_list[::-1]
-            print(line_list)
 
         if is_safe(line_list):
             count += 1
         else:
-            dist = [n1 - n2 for n1, n2 in zip(line_list, line_list[1:])]
-            for i, d in enumerate(dist):
-                if d < 1 or d > 3:
-                    line_list.remove(line_list[i])
+            for i in range(len(line_list)):
+                if is_safe(line_list[:i] + line_list[i + 1 :]):
+                    count += 1
                     break
-            if is_safe(line_list):
-                count += 1
 
     print(count)
 
