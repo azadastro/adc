ce07a9fc0c759fb381f680f6eaba3e02a5b184d7
Azad Astro <azad.astro@gmail.com>
2024-12-29 22:03:07 -0500

    prob 22_2 2024

--- 2024/22.py
+++ 2024/22.py
@@ -26,9 +26,21 @@ def part_one():
 
 
 def part_two(all_secret_nums):
-    all_prices = []
+    seq_2_prices = {}
     for secret_nums in all_secret_nums:
-        all_prices.append([s for s in secret_nums])
+        all_prices = [s % 10 for s in secret_nums]
+        seen = set()
+        for i in range(len(all_prices) - 4):
+            p1, p2, p3, p4, p5 = all_prices[i : i + 5]
+            seq = (p2 - p1, p3 - p2, p4 - p3, p5 - p4)
+            if seq in seen:
+                continue
+            seen.add(seq)
+            if seq not in seq_2_prices:
+                seq_2_prices[seq] = 0
+            seq_2_prices[seq] += p5
+
+    print(max(seq_2_prices.values()))
 
 
 if __name__ == "__main__":
