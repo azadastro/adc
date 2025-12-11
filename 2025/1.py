b6d67196a143d7fa273361ad60451eb6556b6616
Azad Astro <azad.astro@gmail.com>
2025-12-11 12:43:36 -0500

    prob 1_2 2025

--- 2025/1.py
+++ 2025/1.py
@@ -3,25 +3,14 @@ lines = open("2025/inputs/1.txt").read().splitlines()
 
 def part_one(lines):
 
-    initial_state = 50
+    state = 50
     count_zeros = 0
     for line in lines:
-        d = line[0]
-        r = int(line[1:])
-        if r > 100:
-            r = r % 100
-        # print(initial_state, d, r)
-        if d == "L":
-            initial_state -= r
-            if initial_state < 0:
-                initial_state = 100 + initial_state
-        elif d == "R":
-            initial_state += r
-            if initial_state > 99:
-                initial_state = initial_state - 100
+        turn = int(line.replace("L", "-").replace("R", "+"))
 
-        # print(f"Current state: {initial_state}")
-        if initial_state == 0:
+        state = (state + turn) % 100
+
+        if state == 0:
             count_zeros += 1
 
     print(f"Number of times pointing at zero: {count_zeros}")
@@ -30,7 +19,25 @@ def part_one(lines):
 
 def part_two():
 
-    pass
+    state = 50
+    count_zeros_cross = 0
+    for line in lines:
+
+        d = line[0]
+        r = int(line[1:])
+        count_zeros_cross += r // 100
+        r = r % 100
+        if d == "L":
+            if state != 0 and state - r <= 0:
+                count_zeros_cross += 1
+            state = (state - r) % 100
+        elif d == "R":
+            if state + r >= 100:
+                count_zeros_cross += 1
+            state = (state + r) % 100
+            
+    print(f"Number of times pointing at zero: {count_zeros_cross}")
+    return
 
 
 if __name__ == "__main__":
