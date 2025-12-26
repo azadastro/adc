745474fc996372048c003e6002182aed005bae02
Azad Astro <azad.astro@gmail.com>
2025-12-26 17:30:07 -0500

    prob 9_2 2025

--- 2025/9.py
+++ 2025/9.py
@@ -21,9 +21,48 @@ def part_one(tiles):
     print(max_area)
 
 
-def part_two():
+def get_all_xs(tiles):
+    all_xs = {}
+    for (x1, y1), (x2, y2) in zip(tiles, tiles[1:]+tiles[:1]):
+        all_xs.setdefault(y1, set()).add(x1)
+        x1, x2 = sorted([x1, x2])
+        y1, y2 = sorted([y1, y2])
+        if x1 == x2:
+            for y in range(y1+1, y2):
+                all_xs.setdefault(y, set()).add(x1)
+        
+        if y1 == y2:
+            for x in range(x1+1, x2):
+                all_xs.setdefault(y1, set()).add(x)
 
-    pass
+    for k, v in all_xs.items():
+        all_xs[k] = (min(v), max(v))
+        
+    return all_xs
+
+
+def valid_area(all_xs, tile1, tile2):
+    min_x, max_x = sorted([tile1[0], tile2[0]])
+    min_y, max_y = sorted([tile1[1], tile2[1]])
+    for y in range(min_y, max_y+1):
+        x_min, x_max = all_xs[y]
+        if min_x < x_min or max_x > x_max:
+            return False
+    return True
+
+
+def part_two(tiles):
+    
+    all_xs = get_all_xs(tiles)
+
+    max_area = 0
+    for i in range(len(tiles)):
+        for j in range(i + 1, len(tiles)):
+            area = calc_area(tiles[i], tiles[j])
+            if area > max_area and valid_area(all_xs, tiles[i], tiles[j]):
+                max_area = area
+                tiles_pair = (tiles[i], tiles[j])
+    print(max_area)
 
 
 if __name__ == "__main__":
@@ -32,4 +71,4 @@ if __name__ == "__main__":
     part_one(tiles)
 
     print("Part Two:")
-    part_two()
+    part_two(tiles)
